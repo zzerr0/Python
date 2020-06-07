6497ce00698d3d9f6c20d23f1582d3a9cc593899
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-06-07 11:56:45 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -16,3 +16,36 @@ hello()
 Hello("Shivam")
 
   
+
+
+#defining a function without parameter 
+
+def hello():
+
+  print("It is Zero")
+
+  print("We build best quality drones")
+
+  
+
+#defining a function with a parameter
+
+def Hello(name):
+
+  print("My name is ",name)
+
+  print("I own a company name Zero")
+
+hello()  
+
+#parameter written inside function Hello("Shivam")
+
+#will be passed to above function declaration
+
+Hello("Shivam")
+
+print("Creating a blank line with print() function")
+
+print();
+
+Hello("Ashna") 
