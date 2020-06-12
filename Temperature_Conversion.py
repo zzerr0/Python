091a5e8459dd25af0ccbe13ab9fc26d181cf4d8e
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-06-12 08:56:34 +0530

    Create Temperature_Conversion.py

---
+++ Temperature_Conversion.py
@@ -0,0 +1,5 @@
+#Degree Celsius to Fahrenheit Program
+
+celcius = eval(input("Enter the temperature in celsius \n"))
+fahrenheit = (9/5)*celcius+32
+print("The Temperature in Fahrenheit = ",fahrenheit)
