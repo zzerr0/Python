9bfa6b6d87877a41a327a39f2e47ecc08fcd4268
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-06-18 10:29:13 +0530

    Create Inputa.py

---
+++ Inputa.py
@@ -0,0 +1,5 @@
+#program for finding the average of two marks
+print("Program to calculate average ")
+score1, score2 = eval(input("Enter the scores separated by comma like 4,5 \n"))
+average = (score1+score2)/2
+print("The average of two score is ",average, "\n")
