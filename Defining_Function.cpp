e3e1485b15bf6cdbb619d7e8e26432e4fe523e19
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-06-22 18:31:44 +0530

    Create Defining_Function.cpp

---
+++ Defining_Function.cpp
@@ -0,0 +1,14 @@
+#Defninig a function
+
+def name():
+  print("I am Shivam Yadav ")
+  
+def zero():
+  print("I am the owner of ZERO ")
+  
+def main():
+  name()
+  zero()
+  
+print("Blank Space ")
+main()
