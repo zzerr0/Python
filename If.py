34335304c329571ec7896875178dda8d67269aa6
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-06-25 11:19:22 +0530

    Create If.py

---
+++ If.py
@@ -0,0 +1,7 @@
+#if statement
+
+fahrenheit = eval(input("Enter the Fahrenheit "))
+if fahrenheit > 90:
+  print("Your Device Is Warming Up! Switch Off ")
+if fahrenheit <= 90: 
+  print("Your Device Is Normal ")
