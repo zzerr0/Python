0d7ff45adc0ae8671b50aee7b6714da761fe9948
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-03 11:43:52 +0530

    Create MaxofThree_Using_max_Function.py

---
+++ MaxofThree_Using_max_Function.py
@@ -0,0 +1,6 @@
+#Maximum of three numbers uing inbuilt function max()
+
+x1, x2, x3 = eval(input("Enter 3 numbers separated with comma ,"))
+#inbuilt function max() returns the maximum number out of n-numbers
+print("The maximum number out of these numbers",x1, x2, x3, " is ")
+print(max(x1, x2, x3))
