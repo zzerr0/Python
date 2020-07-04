3247ab2c7cd3d16e41263fbf1513a2cf46ffa278
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-04 10:16:06 +0530

    Create WHILE.py

---
+++ WHILE.py
@@ -0,0 +1,10 @@
+#While Loop
+#while loop is called pre-test loop because the condition is 
+#checked before the execution of body
+def main():
+ iterator = 0
+ while(iterator <= 10):
+   print(iterator)
+   iterator = iterator+1
+main() 
+
