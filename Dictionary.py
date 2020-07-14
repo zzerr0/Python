ac881deaa1725cd492bec51fd780768d0bbd6951
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-14 12:58:58 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+name = { "1":"Shivam","2":"Shubham","3":"Shikhar"}
+#x = int(input("Enter roll no. if student to get name "))
+print("Name of the student is ", name["2"])
+#adding new element to dictionary
+name['4'] = 'Sourabh'
+
+print("Name of the student is ", name["4"])
