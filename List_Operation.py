384edfe61b41f1d306f78e97fcbd5099071c0e0a
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-19 12:18:33 +0530

    Create List_Operation.py

---
+++ List_Operation.py
@@ -0,0 +1,25 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'Hero'
+print(motorcycles)
+
+motorcycles.append("Ducati")
+print(motorcycles)
+
+motorcycles.insert(2,'shine')
+print(motorcycles)
+
+del motorcycles[2]
+print(motorcycles)
+
+print("The Popped element is", motorcycles.pop(0))
+
+list = [0, 4, 1, 2, 7]
+print(list)
+print(sorted(list ))
+print(list)
+
+print(list.reverse())
+
+print(len(list))
