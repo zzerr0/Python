b9e06e39e6bf14e5a8d05ae7ad0831e820f4c050
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-23 13:33:40 +0530

    Create ListOfDictionaries.py

---
+++ ListOfDictionaries.py
@@ -0,0 +1,8 @@
+#List of Dictionaries
+person1 = {'shivam':'college', 'shyam': 'job'}
+person2 = {'shubham':'job', 'Ero': 'job'}
+person3 = {'Dipanshi':'school', 'zero': 'job'}
+
+list = [person1, person2, person3]
+for i in list:
+  print(i)
