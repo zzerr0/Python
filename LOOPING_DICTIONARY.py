494bcf6151bf7f9839f29bb041e96c51ae6228ac
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-23 10:37:39 +0530

    Create LOOPING_DICTIONARY.py

---
+++ LOOPING_DICTIONARY.py
@@ -0,0 +1,12 @@
+#dictionary
+student = { '1': '$HIVAM', 2: 'SHUBHAM'}
+student[2] = 'Raju'
+print(student)
+
+#Looping Through Dictionary
+
+company = {'Owner':'$H|√AM YADAV', 'Company':'Z3R0'}
+
+for k, v in company.items():
+  print("\nKey is",k)
+  print("Value is",v)
