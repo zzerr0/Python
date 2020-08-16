a22776d70ba14cafd4fe6b232782dabbb6a98e8e
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-08-16 23:50:54 +0530

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,20 @@
+#Implementing Stack Using List
+
+#creating an empty stack
+
+stack = []
+
+#using append method to add an elements at the top of the stack
+
+stack.append('Shivam')
+stack.append(4)
+stack.append('Yadav')
+
+print(stack)
+
+#using pop method to remove element from the top of the stack
+#elements 
+print(stack.pop())
+
+#removing element from the top of the stack
+print(stack)
