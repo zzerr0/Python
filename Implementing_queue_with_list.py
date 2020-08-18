0c58c6be44b3aef002546b49c49411a61e88df61
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-08-19 00:15:08 +0530

    Create Implementing_queue_with_list.py

---
+++ Implementing_queue_with_list.py
@@ -0,0 +1,19 @@
+#implementing queue with list
+
+#initializing an empty queue
+queue = []
+#inserting elements into the queue
+queue.append('a')
+queue.append('b')
+queue.append('c')
+queue.append('d')
+
+#intial queue
+print("Initial queue is ")
+print(queue)
+
+#removing an element from queue
+queue.pop(0)
+print("Queue after one pop operation is ")
+print(queue)
+
